be94628c35ab5ec402346db51aabec648e10da2f
BostjanStupica <bostjan@stupica.com>
2019-03-08 21:02:55 +0100

    Git/Project reorganization

---
+++ 11helloWorldObjC/11helloWorldObjC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  11helloWorldObjC
+//
+//  Created by Bostjan Stupica on 23/02/16.
+//  Copyright (c) 2016 stupica.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World! - 01");
+    }
+    return 0;
+}

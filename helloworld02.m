90baaf6a3140709e352600262b05cb608c103590
BostjanStupica <bostjan@stupica.com>
2019-03-09 09:27:04 +0100

    Create helloworld02.m

---
+++ helloworld02.m
@@ -0,0 +1,19 @@
+//
+//  helloworld02.m
+//  11helloWorldObjC
+//
+//  Created by Bostjan Stupica on 23/02/16.
+//  Copyright (c) 2016 stupica.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+int main (int argc, const char * argv[]) {
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    
+    // insert code here...
+    NSLog(@"Hello, World! - 02");
+    [pool drain];
+    return 0;
+}
